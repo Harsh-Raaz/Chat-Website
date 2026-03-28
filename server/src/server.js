34d35d73fa5a29bda60d0651676d129ee2ef5179
Harsh-Raaz <harshrajforofficial@gmail.com>
2026-03-29 00:23:24 +0530

    Added Cloudinary image upload for profile pic

--- server/src/server.js
+++ server/src/server.js
@@ -16,14 +16,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-connectDB();
+
 app.use("/api",authrouter);
 app.use((err, req, res, next) => {
-    console.error(err);
-    res.status(500).json({ message: "Internal Server Error" });
+  console.error(err.stack);
+  res.status(500).json({ message: err.message || "Internal Server Error" });
 });
 
 const PORT=process.env.PORT || 4000;
-app.listen(PORT,()=>{
-    console.log(`Server running on PORT ${PORT}`);
-})
+const startServer = async () => {
+  try {
+    await connectDB();
+
+    app.listen(PORT, () => {
+      console.log(`Server running on PORT ${PORT}`);
+    });
+
+  } catch (error) {
+    console.error("Server start failed:", error.message);
+  }
+};
+console.log(process.env.CLOUD_API_KEY);
+
+startServer();
