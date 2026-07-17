a0a7d1d32e68706753891d48c899f98ca8669b40
Harsh Raj <harshraj990545@gmail.com>
2026-07-17 13:47:31 +0530

    fixed auth api production url

--- client/src/store/authStore.jsx
+++ client/src/store/authStore.jsx
@@ -7,7 +7,7 @@ import {
     uploadProfilePicture,
 } from "../services/api";
 
-axios.defaults.baseURL = 'http://localhost:4000/api';
+axios.defaults.baseURL = 'https://chat-website-backend-holo.onrender.com/api';
 axios.defaults.withCredentials = true;
 
 export const useAuthStore = create((set) => ({
