c424b1ba570cb800c57e76506ee2ce727656a2a1
Harsh Raj <harshraj990545@gmail.com>
2026-07-17 12:20:05 +0530

    added deployed backend link

--- client/src/socket/socket.js
+++ client/src/socket/socket.js
@@ -1,7 +1,7 @@
 // client/src/socket/socket.js
 import { io } from "socket.io-client";
 
-export const socket = io("http://localhost:4000", {
+export const socket = io("https://chat-website-backend-holo.onrender.com", {
   autoConnect: false,
   withCredentials: true,
 });
